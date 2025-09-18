75513b364035d52c0ecdd1677e86894a446c7074
eching0519 <eching0519@gmail.com>
2025-09-18 00:39:10 -0600

    Update web title

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,7 @@ import { HomeView } from 'src/sections/_home/view/home-view';
 // ----------------------------------------------------------------------
 
 const metadata = {
-  title: 'YanniDev: A portfolio of a passionate developer',
+  title: 'Yannis Ng Portfolio',
 };
 
 export default function Page() {
